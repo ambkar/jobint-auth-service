3eebd375a5549d3096d879f91645001ce9343945
ambkar <Ambkar2102@yandex.ru>
2025-07-01 12:58:53 +0300

    fff

--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase
+# from sqlalchemy.orm import DeclarativeBase
 
 
-class Base(DeclarativeBase):  # type: ignore[misc]
-    """Базовый класс для всех ORM-моделей."""
+# class Base(DeclarativeBase):  # type: ignore[misc]
+#     """Базовый класс для всех ORM-моделей."""
+
+from sqlalchemy.ext.declarative import declarative_base  # для 1.4
+
+Base = declarative_base()
