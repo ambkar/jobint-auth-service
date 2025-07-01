560147fdc89b9f0611f0b449dc486789c01ed9b8
ambkar <Ambkar2102@yandex.ru>
2025-07-01 10:10:24 +0300

    klsd

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from sanic_ext import Extend
 from app.api.v1.auth_routes import auth_bp
 from app.api.v1.user_routes import user_bp
 from app.core.config import settings
+from sanic.response import json 
 
 app = Sanic(settings.project_name)          # --- app уже создан
 Extend(app)
@@ -13,9 +14,9 @@ app.blueprint(user_bp)
 
 @app.get("/health")
 async def health(_):
-    return app.json({"status": "ok"})
+    return json({"status": "ok"})
 
 
 if __name__ == "__main__":
     # dev-режим, без форков
-    app.run(host="0.0.0.0", port=8001, workers=1)
+    app.run(host="0.0.0.0", port=8001, workers=1, dev=True)
