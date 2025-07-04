40f00e7e619232ee6c49da8e2f31b66e8ecb0879
ambkar <Ambkar2102@yandex.ru>
2025-07-04 15:58:51 +0300

    jnsdkjd

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field
-
+from typing import Optional
 
 class RegisterIn(BaseModel):
     name: str = Field(max_length=100)
@@ -8,7 +8,10 @@ class RegisterIn(BaseModel):
     phone: str = Field(max_length=20)
     email: EmailStr
     password: str = Field(min_length=8)
-
+    avatar: Optional[str] = Field(
+        default=None,
+        description="Base64-encoded avatar image"
+    )
 
 class LoginIn(BaseModel):
     email: EmailStr
