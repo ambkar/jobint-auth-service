d1703f6155e677332af9d6ddb1bf822166962f4f
ambkar <Ambkar2102@yandex.ru>
2025-07-14 00:55:04 +0300

    kjds

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ async def cleanup_temp_codes(request, response):
     await clear_expired_codes()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8001)
